5b7858f7e044d51eba3992a7a4d81a9d1c64e361
Juan Esquivias <juanesquiviasotazu@gmail.com>
2017-04-06 12:00:39 -0500

    Integrate Firebase and add authentication

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
 import App from './App';
 import './index.css';
 
+firebase.initializeApp({
+	apiKey: "AIzaSyAAjOZ0qfIQLIH6AEDiDpMye3cLQdfRfbU",
+	authDomain: "funnygram-250a7.firebaseapp.com",
+	databaseURL: "https://funnygram-250a7.firebaseio.com",
+	projectId: "funnygram-250a7",
+	storageBucket: "funnygram-250a7.appspot.com",
+	messagingSenderId: "87031868739"
+});
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+		<App/>, document.getElementById('root'));
