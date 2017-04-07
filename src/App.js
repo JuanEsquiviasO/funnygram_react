d03a408c3944f9f0841fe5a2d7aceb947b5da2f7
Juan Esquivias <juanesquiviasotazu@gmail.com>
2017-04-07 13:18:09 -0500

    storing data with Firebase realtime database

--- src/App.js
+++ src/App.js
@@ -8,17 +8,25 @@ class App extends Component {
 	constructor () {
 		super();
 		this.state = {
-			user: null
+			user: null,
+			pictures: []
 		};
 
 		this.handleAuth = this.handleAuth.bind(this);
 		this.handleLogout = this.handleLogout.bind(this);
+		this.handleUpload = this.handleUpload.bind(this);
 	}	
 	
 	componentWillMount () {
 		firebase.auth().onAuthStateChanged(user => {
 			this.setState({ user });
-		})
+		});
+
+		firebase.database().ref('pictures').on('child_added', snapshot => {
+			this.setState({
+				pictures: this.state.pictures.concat(snapshot.val())
+			});
+		});
 	}
 	
 	handleAuth () {
@@ -35,6 +43,31 @@ class App extends Component {
 		.catch(error => console.log(`Error ${error.code}: ${error.message}`));
 	}
 
+	handleUpload (event) {
+		const file = event.target.files[0];
+		const storageRef = firebase.storage().ref(`/Photos/${file.name}`);
+		const task = storageRef.put(file);
+
+		task.on('state_changed', snapshot => {
+			let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+			this.setState({
+				uploadValue: percentage
+			})
+		}, error => { 
+			console.log(error.message)
+		}, () => {
+			const record = {
+				photoURL: this.state.user.photoURL,
+				displayName: this.state.user.displayName,
+				image: task.snapshot.downloadURL
+			};
+
+			const dbRef = firebase.database().ref('pictures');
+			const newPicture = dbRef.push();
+			newPicture.set(record);
+		});
+	}
+
 	renderLoginButton () {
 		if (this.state.user) {
 			return (
@@ -42,7 +75,21 @@ class App extends Component {
 					<img src={this.state.user.photoURL} alt={this.state.user.displayName}/>
 					<p>Hi {this.state.user.displayName}!</p>
 					<button onClick={this.handleLogout}>Exit</button>
-					<FileUpload />
+
+					<FileUpload onUpload={ this.handleUpload } />
+
+					{
+						this.state.pictures.map(picture => (
+							<div>
+								<img src={picture.image} />
+								<br/>
+								<img width="50" src={picture.photoURL} alt={picture.displayName}/>
+								<br/>
+								<span>{picture.displayName}</span>
+							</div>
+						)).reverse()
+					}
+
 				</div>
 			);
 		} else {
